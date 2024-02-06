d6fc7d91ed3f7423af4e1303835af2a7512dae3b
maxiamikel <maxloversist@gmail.com>
2024-02-06 12:12:38 -0300

    Initial configuration, conntction to the DBA, creation of project main folders

---
+++ src/main/java/com/maxi/backstock/BackStockApplication.java
@@ -0,0 +1,13 @@
+package com.maxi.backstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maxi/backstock/BackStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxi.backstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
